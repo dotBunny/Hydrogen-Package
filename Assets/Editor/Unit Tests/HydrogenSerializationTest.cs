7911c6df3f198bf3a6c5827c13daea8229498f2b
Matthew Davey <matthew.davey@dotbunny.com>
2013-11-27 07:29:11 -0500

    Start to work on Input example and a serialization unit test.

---
+++ Assets/Editor/Unit Tests/HydrogenSerializationTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Hydrogen;
+
+[NUnit.Framework.TestFixtureAttribute]
+public class HydrogenSerializationTest {
+	
+	private System.Text.StringBuilder _file;
+	private string _processed;
+
+
+	private Dictionary<string,string> _expectedData; 
+	private Dictionary<string,string> _processedData;
+	private bool _check;
+	
+	#region Hydrogen.Serialization.INI
+	[NUnit.Framework.TestAttribute]
+	public void Serialize_INI_DeserializeOneLine()
+	{
+		_file = new System.Text.StringBuilder();
+		_file.AppendLine("Key=Value");
+
+		_processedData = Hydrogen.Serialization.INI.Deserialize(_file.ToString(), '=');
+	
+		_expectedData = new Dictionary<string, string>();
+		_expectedData.Add("Key", "Value");
+
+		if ( _expectedData.IsDictionaryEqual(_processedData) ) _check = true;
+
+		NUnit.Framework.Assert.AreEqual(true, _check, "Hydrogen.Serialization.INI.DeserializeOneLine");
+	}
+
+	[NUnit.Framework.TestAttribute]
+	public void Serialize_INI_SerializeOneLine()
+	{
+		_file = new System.Text.StringBuilder();
+		_file.AppendLine("Key=Value");
+
+		_expectedData = new Dictionary<string, string>();
+		_expectedData.Add("Key", "Value");
+		
+		_processed = Hydrogen.Serialization.INI.Serialize(_expectedData, '=');
+
+
+		NUnit.Framework.Assert.AreEqual(_file.ToString(), _processed, "Hydrogen.Serialization.INI.SerializeOneLine");
+	}
+
+	#endregion
+}
---
+++ Assets/Hydrogen Framework/Examples/Source/InputExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Hydrogen;
+
+public class InputExample : MonoBehaviour
+{
+	public GameObject turret;
+	public GameObject muzzle;
+	public GameObject[] ammo;
+
+
+	void Awake()
+	{
+	}
+
+	void Start()
+	{
+		hInput.Instance.AddAction("Move", OnMove);
+		hInput.Instance.AddAction("Turn", OnTurn);
+		hInput.Instance.AddAction("Rotate", OnRotate);
+		hInput.Instance.AddAction("Shoot", OnShoot);
+		
+		hInput.Instance.AddControl("Mouse X", "Rotate");
+		hInput.Instance.AddControl("Horizontal", "Turn");
+		hInput.Instance.AddControl("Vertical", "Move");
+		hInput.Instance.AddControl("Left", "Shoot");
+		hInput.Instance.AddControl("Space", "Shoot");
+	}
+	
+	private void OnRotate(Hydrogen.Peripherals.InputEvent evt, float value, float time)
+	{
+		// Mouse X Axes are relative movements only. So we only turn the turret - never directly set the rotation.
+		turret.transform.localRotation *= Quaternion.AngleAxis(value * 180.0f * Time.deltaTime, Vector3.up);
+	}
+	
+	private void OnShoot(Hydrogen.Peripherals.InputEvent evt, float value, float time)
+	{
+		if (evt == Hydrogen.Peripherals.InputEvent.Released)
+		{
+		//	GameObject shell = Instantiate(ammo, muzzle.transform.position, Quaternion.identity) as GameObject;
+		//	shell.rigidbody.velocity = turret.transform.rotation * Vector3.forward * 10.0f * (time * time + 0.1f);
+		}
+	}
+	
+	private void OnMove(Hydrogen.Peripherals.InputEvent evt, float value, float time)
+	{
+		// Vertical Axes give it's state of absolute values of -1.0, 1.0 only.
+		// So this is used to move the box along it's looking direction (horizontal). This will handle forward and backward movement in one go.
+		transform.transform.position += (transform.rotation * transform.forward) * value * 4.0f * Time.deltaTime;
+	}
+	
+	private void OnTurn(Hydrogen.Peripherals.InputEvent evt, float value, float time)
+	{
+		// Horizontal Axes give it's state of absolute values of -1.0, 1.0 only.
+		// We normalised this to a steering range, then use this to turn the tank.
+		transform.transform.localRotation *= Quaternion.AngleAxis(value * 90.0f * Time.deltaTime, Vector3.up);
+	}
+
+}
